45932b5de5ead3545d1c3a0a698d1a80657f1268
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-06-25 00:16:26 +0430

    cleanup code

--- solver.rb
+++ solver.rb
@@ -13,17 +13,10 @@ class Solver
     def fizzbuzz(num)
         mod3 = num % 3
         mod5 = num % 5
-        return 'fizzbuzz' if mod3? && mod5?
-        return 'fizz' if mod3?
-        return 'buzz' if mod5?
+        return 'fizzbuzz' if mod3 && mod5
+        return 'fizz' if mod3
+        return 'buzz' if mod5
         return "num is #{num}"
     end
 
 end
-# fact = Solver.new
-# puts fact.factorial(2)
-# reverse = Solver.new
-# puts reverse.reverse('num')
-# fizbuzz = Solver.new
-# puts fizbuzz.fizzbuzz(2)
-
