1dd6e8c6c3dc0e5557249aa9d2989a737d7f0744
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-06-24 21:46:03 +0430

    create methods functionality

--- solver.rb
+++ solver.rb
@@ -1,16 +1,29 @@
 class Solver
-    attr_accessor :factorial, :reverse, :fizzbuzz
-
-    def initialize
         
-    
-    def factorial
+    def factorial(num)
+       raise 'no Negative number' if num.negative?
+       return 1 if [1, 0].include?(num)
+       factorial(num -1) * num
     end
 
-    def reverse
+    def reverse(string)
+        string.reverse
     end
 
-    def fizzbuzz
+    def fizzbuzz(num)
+        mod3 = num % 3
+        mod5 = num % 5
+        return 'fizzbuzz' if mod3.zero? && mod5.zero?
+        return 'fizz' if mod3.zero?
+        return 'buzz' if mod5.zero?
+        return "num is #{num}"
     end
 
 end
+fact = Solver.new
+puts fact.factorial(2)
+reverse = Solver.new
+puts reverse.reverse('num')
+fizbuzz = Solver.new
+puts fizbuzz.fizzbuzz(2)
+
