eccca36c5acd11dab065fd46c4d887aec5c42cc3
Have Samuel <samiehave9@gmail.com>
2022-06-24 23:24:59 +0300

    Fixing Lnters

--- solver.rb
+++ solver.rb
@@ -1,21 +1,21 @@
 class Solver
-       
-    def factorial(num)
-       raise 'no Negative number' if num.negative?
-       return 1 if [1, 0].include?(num)
-       factorial(num -1) * num
-    end
+  def factorial(num)
+    raise 'no Negative number' if num.negative?
+    return 1 if [1, 0].include?(num)
 
-    def reverse(string)
-        string.reverse
-    end
+    factorial(num - 1) * num
+  end
 
-    def fizzbuzz(num)
-        mod3 = num % 3
-        mod5 = num % 5
-        return 'fizzbuzz' if mod3.zero? && mod5.zero?
-        return 'fizz' if mod3.zero?
-        return 'buzz' if mod5.zero?
-        return num.to_s
-    end
+  def reverse(string)
+    string.reverse
+  end
+
+  def fizzbuzz(num)
+    mod3 = num % 3
+    mod5 = num % 5
+    return 'fizzbuzz' if mod3.zero? && mod5.zero?
+    return 'fizz' if mod3.zero?
+    return 'buzz' if mod5.zero?
+    return num.to_s
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,27 +1,26 @@
 require_relative '../solver'
 
 describe Solver do
-
   before :all do
     @solver = Solver.new
   end
 
   describe 'factorial' do
     it 'raise error with negative number' do
-        num = -2
-        expect { @solver.factorial(num).to raise_error('no Negative number')}
+    num = -2
+      expect { @solver.factorial(num).to raise_error('no Negative number') }
     end
 
     it 'return the factorial of the positive number passed in' do
-        number = 3
-        expect(@solver.factorial(number)).to eq 6
+    number = 3
+      expect(@solver.factorial(number)).to eq 6
     end
   end
 
   describe 'reverse' do
     it 'reversed a string' do
-        string = 'statement'
-        expect(@solver.reverse(string)).to eq 'tnemetats'
+    string = 'statement'
+      expect(@solver.reverse(string)).to eq 'tnemetats'
     end
   end
 
