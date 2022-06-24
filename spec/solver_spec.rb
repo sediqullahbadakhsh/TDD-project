24931152b44517001521d9f67d95f3646d8c766c
Have Samuel <samiehave9@gmail.com>
2022-06-24 23:44:39 +0300

    Fixing Lnters

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -8,18 +8,18 @@ describe Solver do
   describe 'factorial' do
     it 'raise error with negative number' do
       num = -2
-      expect{ @solver.factorial(num).to raise_error('no Negative number') }
+      expect { @solver.factorial(num).to raise_error('no Negative number') }
     end
 
     it 'return the factorial of the positive number passed in' do
       number = 3
-      expect(@solver.factorial(number)).to eq('6')
+      expect(@solver.factorial(number)).to eq ('6')
     end
   end
 
   describe 'reverse' do
     it 'reversed a string' do
-    string = 'statement'
+      string = 'statement'
       expect(@solver.reverse(string)).to eq('tnemetats')
     end
   end
@@ -38,7 +38,7 @@ describe Solver do
     end
 
     it 'return number as a string if not divisible by 3 or 5' do
-      expect(@solver.fizzbuzz(4)).to eq ('4')
+      expect(@solver.fizzbuzz(4)).to eq('4')
     end
   end
 end
